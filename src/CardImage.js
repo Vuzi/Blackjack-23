a9f44b6ba63724a117409220a76dbe89d2aff7b0
Mathieu Péquin <mat.pequin@gmail.com>
2016-03-03 16:48:10 +0100

    Cards png file and card gen update

--- src/CardImage.js
+++ src/CardImage.js
@@ -2,7 +2,7 @@ import React from "react";
 import CardImageGenerator from "./CardImageGenerator";
 
 // Image generator for card image
-const cardImageGenerator = new CardImageGenerator('images/cards.jpg', 225, 315, ['heart', 'spade', 'diamond', 'club']);
+const cardImageGenerator = new CardImageGenerator('images/cards.png', 167.5384615384615 , 243.2, ['club', 'diamond', 'heart', 'spade']);
 
 
 // Image of a card, based on a card value, using the card image factory
